ad232d0f31114ae330bb02097ba735c59baf143c
skgone <natureguzhiming@gmail.com>
2019-07-07 23:44:41 +0800

    change name to skgone

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-name = 'Grey Li'
+name = 'skgone'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
